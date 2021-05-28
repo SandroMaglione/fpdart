937b25aee4ee859871f75363c6e1074d92b4f996
Sandro Maglione <lass.maglio@gmail.com>
2021-05-28 17:37:28 +0200

    Eq

---
+++ lib/src/Eq.dart
@@ -0,0 +1,64 @@
+/// A type class used to determine equality between 2 instances **of the same type `T`**.
+/// Any 2 instances `x` and `y` are equal if `eqv(x, y)` is `true`.
+///
+/// Moreover, `eqv` should form an [equivalence relation](https://en.wikipedia.org/wiki/Equivalence_relation)
+/// (a binary relation that is reflexive, symmetric, and transitive).
+abstract class Eq<T> {
+  const Eq();
+
+  /// Returns `true` if `x` and `y` are equivalent, `false` otherwise.
+  bool eqv(T x, T y);
+
+  /// Returns `false` if `x` and `y` are equivalent, `true` otherwise.
+  bool neqv(T x, T y) => !eqv(x, y);
+
+  /// Convert an implicit `Eq<B>` to an `Eq<A>` using the given function `f`.
+  ///
+  /// ```dart
+  /// /// Convert an `Eq` on `int` to an `Eq` to check `String` length
+  /// final instance = Eq.instance<int>((a1, a2) => a1 == a2);
+  /// final by = Eq.by<String, int>((a) => a.length, instance);
+  ///
+  /// expect(by.eqv('abc', 'abc'), true); // Same length
+  /// expect(by.eqv('abc', 'ab'), false); // Different length
+  /// ```
+  static Eq<A> by<A, B>(B Function(A a) f, Eq<B> eq) =>
+      _Eq((x, y) => eq.eqv(f(x), f(y)));
+
+  /// Return an `Eq` that gives the result of the **and** of `eq1` and `eq2`.
+  ///
+  /// Note this is idempotent.
+  static Eq<A> and<A>(Eq<A> eq1, Eq<A> eq2) =>
+      _Eq((x, y) => eq1.eqv(x, y) && eq2.eqv(x, y));
+
+  /// Return an `Eq` that gives the result of the **or** of `eq1` and `eq2`.
+  ///
+  /// Note this is idempotent.
+  static Eq<A> or<A>(Eq<A> eq1, Eq<A> eq2) =>
+      _Eq((x, y) => eq1.eqv(x, y) || eq2.eqv(x, y));
+
+  /// Create an `Eq` instance from an `eqv` implementation.
+  ///
+  /// ```dart
+  /// final instance = Eq.instance<String>((a1, a2) => a1.substring(0, 2) == a2.substring(0, 2));
+  ///
+  /// expect(instance.eqv('abc', 'abc'), true); // Same 2 characters prefix
+  /// expect(instance.eqv('abc', 'acb'), false); // Different 2 characters prefix
+  /// ```
+  static Eq<A> instance<A>(bool Function(A a1, A a2) f) => _Eq(f);
+
+  /// An `Eq<A>` that delegates to universal equality (`==`).
+  static Eq<A> fromUniversalEquals<A>() => _Eq((x, y) => x == y);
+
+  /// An `Eq<A>` in which everything is the same
+  /// (calling `eqv` returns always `true`).
+  static Eq<A> allEqual<A>() => _Eq((x, y) => true);
+}
+
+class _Eq<T> extends Eq<T> {
+  final bool Function(T x, T y) eq;
+  const _Eq(this.eq);
+
+  @override
+  bool eqv(T x, T y) => eq(x, y);
+}
---
+++ test/src/Eq_test.dart
@@ -0,0 +1,70 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Eq', () {
+    test('.instance (int)', () async {
+      final instance = Eq.instance<int>((a1, a2) => a1 == (a2 + 1));
+
+      // eqv
+      expect(instance.eqv(1, 1), false);
+      expect(instance.eqv(2, 1), true);
+      expect(instance.eqv(3, 1), false);
+
+      // neqv
+      expect(instance.neqv(1, 1), true);
+      expect(instance.neqv(2, 1), false);
+      expect(instance.neqv(3, 1), true);
+    });
+
+    test('.instance (String)', () async {
+      final instance = Eq.instance<String>(
+          (a1, a2) => a1.substring(0, 2) == a2.substring(0, 2));
+      expect(instance.eqv('abc', 'abc'), true);
+      expect(instance.eqv('abc', 'acb'), false);
+    });
+
+    test('.and', () {
+      final instance1 = Eq.instance<String>(
+          (a1, a2) => a1.substring(0, 2) == a2.substring(0, 2));
+      final instance2 = Eq.instance<String>(
+          (a1, a2) => a1.substring(2, 4) == a2.substring(2, 4));
+      final and = Eq.and(instance1, instance2);
+      expect(instance1.eqv('abef', 'abcd'), true);
+      expect(instance2.eqv('abef', 'zxef'), true);
+      expect(and.eqv('abcd', 'abcd'), true);
+      expect(and.eqv('abdc', 'abcd'), false);
+      expect(and.eqv('bacd', 'abcd'), false);
+    });
+
+    test('.or', () {
+      final instance1 = Eq.instance<int>((a1, a2) => a1 == (a2 + 2));
+      final instance2 = Eq.instance<int>((a1, a2) => a1 == (a2 + 3));
+      final or = Eq.or(instance1, instance2);
+      expect(or.eqv(2, 1), false);
+      expect(or.eqv(3, 1), true);
+      expect(or.eqv(4, 1), true);
+      expect(or.eqv(5, 1), false);
+    });
+
+    test('.fromUniversalEquals', () {
+      final instance = Eq.fromUniversalEquals<int>();
+      expect(instance.eqv(1, 1), true);
+      expect(instance.eqv(1, 2), false);
+    });
+
+    test('.allEqual', () {
+      final instance = Eq.allEqual<int>();
+      expect(instance.eqv(1, 1), true);
+      expect(instance.eqv(1, 2), true);
+      expect(instance.eqv(2, 1), true);
+    });
+
+    test('.by', () {
+      final instance = Eq.instance<int>((a1, a2) => a1 == a2);
+      final by = Eq.by<String, int>((a) => a.length, instance);
+      expect(by.eqv('abc', 'abc'), true);
+      expect(by.eqv('abc', 'ab'), false);
+    });
+  });
+}
