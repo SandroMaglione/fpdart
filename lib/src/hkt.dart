2112ff6fd89803329e6902bac08a6799e14063d0
Sandro Maglione <lass.maglio@gmail.com>
2021-06-05 18:30:08 +0200

    Higher-kinded type

---
+++ lib/src/hkt.dart
@@ -0,0 +1,10 @@
+/// https://marcosh.github.io/post/2020/04/15/higher-kinded-types-php-solution.html
+/// https://medium.com/@gcanti/higher-kinded-types-in-flow-275b657992b7
+
+/// - [A]: Type parameter
+/// - [G]: Type constructor
+///
+/// Instead of writing `G<A>`, we will write `HKT<G, A>`.
+/// This is useful because in the expression `HKT<G, A>`, both `G` and `A` have kind `*`,
+/// so we can deal with them with the type system we currently have at our disposal.
+abstract class HKT<G, A> {}
