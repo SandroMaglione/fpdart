d4d356b71a8e557017e327f181c689200dd751c0
Sandro Maglione <lass.maglio@gmail.com>
2021-06-05 18:30:27 +0200

    Functor

---
+++ lib/src/functor.dart
@@ -0,0 +1,9 @@
+import 'hkt.dart';
+
+/// `Functor<G, A> extends HKT<G, A>` to express the fact that the classes implementing
+/// the [Functor] interface will need to be higher kinded types.
+abstract class Functor<G, A> extends HKT<G, A> {
+  /// Return type is `HKT<G, B>`, which expresses the fact that what
+  /// we return is using exactly the same type constructor represented by the brand `G`
+  HKT<G, B> map<B>(B Function(A a) f);
+}
