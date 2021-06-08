938209a5bb1088e6baffb46f77208970cccccb47
Sandro Maglione <lass.maglio@gmail.com>
2021-06-08 07:55:21 +0200

    TaskEither

---
+++ lib/src/task_either.dart
@@ -0,0 +1,49 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:fpdart/src/task.dart';
+
+class TaskEither<L, R> extends Task<Either<L, R>> {
+  TaskEither(Future<Either<L, R>> Function() run) : super(run);
+
+  TaskEither<R, L> swap() => TaskEither(
+      () async => (await run()).match((l) => Right(l), (r) => Left(r)));
+
+  TaskEither<L, R> alt(TaskEither<L, R> Function() then) => TaskEither(
+      () async => (await run()).match((_) => then().run(), (_) => run()));
+
+  TaskEither<TL, R> orElse<TL>(TaskEither<TL, R> Function(L l) orElse) =>
+      TaskEither(() async => (await run()).match(
+          (l) => orElse(l).run(), (r) => TaskEither<TL, R>.right(r).run()));
+
+  Task<R> getOrElse(R Function(L l) orElse) =>
+      Task(() async => (await run()).match(orElse, identity));
+
+  Task<A> match<A>(A Function(L l) onLeft, A Function(R r) onRight) =>
+      Task(() async => (await run()).match(onLeft, onRight));
+
+  factory TaskEither.right(R right) => TaskEither(() async => Right(right));
+  factory TaskEither.left(L left) => TaskEither(() async => Left(left));
+
+  factory TaskEither.fromTask(Task<R> task) =>
+      TaskEither(() async => Right(await task.run()));
+
+  factory TaskEither.fromPredicate(
+          R value, bool Function(R a) predicate, L Function(R a) onFalse) =>
+      TaskEither(
+          () async => predicate(value) ? Right(value) : Left(onFalse(value)));
+
+  factory TaskEither.fromMaybe(Maybe<R> maybe, L Function() onNothing) =>
+      TaskEither(() async =>
+          maybe.match((just) => Right(just), () => Left(onNothing())));
+
+  factory TaskEither.fromEither(Either<L, R> either) =>
+      TaskEither(() async => either);
+
+  factory TaskEither.tryCatch(Future<R> Function() run, L Function() onError) =>
+      TaskEither<L, R>(() async {
+        try {
+          return Right<L, R>(await run());
+        } catch (_) {
+          return Left<L, R>(onError());
+        }
+      });
+}
---
+++ test/src/task_either_test.dart
@@ -0,0 +1,159 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:fpdart/src/task.dart';
+import 'package:fpdart/src/task_either.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('TaskEither', () {
+    group('tryCatch', () {
+      test('Success', () async {
+        final task = TaskEither<String, int>.tryCatch(
+            () => Future.value(10), () => 'error');
+        final r = await task.run();
+        r.match((l) => null, (r) => expect(r, 10));
+      });
+
+      test('Failure', () async {
+        final task = TaskEither<String, int>.tryCatch(
+            () => Future.error(10), () => 'error');
+        final r = await task.run();
+        r.match((l) => expect(l, 'error'), (r) => null);
+      });
+    });
+
+    group('fromEither', () {
+      test('Right', () async {
+        final task = TaskEither<String, int>.fromEither(Right(10));
+        final r = await task.run();
+        r.match((l) => null, (r) => expect(r, 10));
+      });
+
+      test('Left', () async {
+        final task = TaskEither<String, int>.fromEither(Left('error'));
+        final r = await task.run();
+        r.match((l) => expect(l, 'error'), (r) => null);
+      });
+    });
+
+    group('fromMaybe', () {
+      test('Just', () async {
+        final task = TaskEither<String, int>.fromMaybe(Just(10), () => 'none');
+        final r = await task.run();
+        r.match((l) => null, (r) => expect(r, 10));
+      });
+
+      test('Nothing', () async {
+        final task = TaskEither<String, int>.fromMaybe(Nothing(), () => 'none');
+        final r = await task.run();
+        r.match((l) => expect(l, 'none'), (r) => null);
+      });
+    });
+
+    group('fromPredicate', () {
+      test('True', () async {
+        final task = TaskEither<String, int>.fromPredicate(
+            20, (n) => n > 10, (n) => '$n');
+        final r = await task.run();
+        r.match((l) => null, (r) => expect(r, 20));
+      });
+
+      test('False', () async {
+        final task = TaskEither<String, int>.fromPredicate(
+            10, (n) => n > 10, (n) => '$n');
+        final r = await task.run();
+        r.match((l) => expect(l, '10'), (r) => null);
+      });
+    });
+
+    test('fromTask', () async {
+      final task = TaskEither<String, int>.fromTask(Task(() async => 10));
+      final r = await task.run();
+      r.match((l) => null, (r) => expect(r, 10));
+    });
+
+    test('left', () async {
+      final task = TaskEither<String, int>.left('none');
+      final r = await task.run();
+      r.match((l) => expect(l, 'none'), (r) => null);
+    });
+
+    test('right', () async {
+      final task = TaskEither<String, int>.right(10);
+      final r = await task.run();
+      r.match((l) => null, (r) => expect(r, 10));
+    });
+
+    group('match', () {
+      test('Right', () async {
+        final task = TaskEither<String, int>(() async => Right(10));
+        final ex = task.match((l) => l.length, (r) => r + 10);
+        final r = await ex.run();
+        expect(r, 20);
+      });
+
+      test('Left', () async {
+        final task = TaskEither<String, int>(() async => Left('none'));
+        final ex = task.match((l) => l.length, (r) => r + 10);
+        final r = await ex.run();
+        expect(r, 4);
+      });
+    });
+
+    group('getOrElse', () {
+      test('Right', () async {
+        final task = TaskEither<String, int>(() async => Right(10));
+        final ex = task.getOrElse((l) => l.length);
+        final r = await ex.run();
+        expect(r, 10);
+      });
+
+      test('Left', () async {
+        final task = TaskEither<String, int>(() async => Left('none'));
+        final ex = task.getOrElse((l) => l.length);
+        final r = await ex.run();
+        expect(r, 4);
+      });
+    });
+
+    group('orElse', () {
+      test('Right', () async {
+        final task = TaskEither<String, int>(() async => Right(10));
+        final ex =
+            task.orElse<int>((l) => TaskEither(() async => Right(l.length)));
+        final r = await ex.run();
+        r.match((l) => null, (r) => expect(r, 10));
+      });
+
+      test('Left', () async {
+        final task = TaskEither<String, int>(() async => Left('none'));
+        final ex =
+            task.orElse<int>((l) => TaskEither(() async => Right(l.length)));
+        final r = await ex.run();
+        r.match((l) => null, (r) => expect(r, 4));
+      });
+    });
+
+    group('alt', () {
+      test('Right', () async {
+        final task = TaskEither<String, int>(() async => Right(10));
+        final ex = task.alt(() => TaskEither(() async => Right(20)));
+        final r = await ex.run();
+        r.match((l) => null, (r) => expect(r, 10));
+      });
+
+      test('Left', () async {
+        final task = TaskEither<String, int>(() async => Left('none'));
+        final ex = task.alt(() => TaskEither(() async => Right(20)));
+        final r = await ex.run();
+        r.match((l) => null, (r) => expect(r, 20));
+      });
+    });
+
+    test('swap', () async {
+      final task = TaskEither<String, int>(() async => Right(10));
+      final ex = task.swap();
+      final r = await ex.run();
+      r.match((l) => expect(l, 10), (r) => null);
+    });
+  });
+}
