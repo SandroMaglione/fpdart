7606f5b16e5f473c79e3e94c4082090b9252c2cb
Sandro Maglione <lass.maglio@gmail.com>
2021-06-11 21:39:08 +0200

    Curry

---
+++ example/src/function/curry.dart
@@ -0,0 +1,25 @@
+import 'package:fpdart/fpdart.dart';
+
+int sum(int param1, int param2) => param1 + param2;
+
+double sumMultiplyDivide(int param1, int param2, int param3, int param4) =>
+    (param1 + param2) * param3 / param4;
+
+void main() {
+  /// Convert a function with 2 parameters to a function that
+  /// takes the first parameter and returns a function that takes
+  /// the seconds parameter.
+  final sumCurry = curry2(sum);
+  final sumBy2 = sumCurry(2);
+  final sumBy10 = sumCurry(10);
+  print(sumBy2(10));
+  print(sumBy10(2));
+
+  /// Same as above but with 4 parameters.
+  final sumMultiplyDivideCurry = curry4(sumMultiplyDivide);
+  final sumBy5 = sumMultiplyDivideCurry(5);
+  final multiplyBy2 = sumBy5(2);
+  final divideBy3 = multiplyBy2(3);
+  print(divideBy3(10));
+  print(sumMultiplyDivideCurry(5)(2)(3)(10));
+}
--- lib/src/function.dart
+++ lib/src/function.dart
@@ -2,3 +2,35 @@ typedef Endo<A> = A Function(A a);
 
 /// Returns the given `a`.
 T identity<T>(T a) => a;
+
+/// Converts a binary function into a unary function that returns a unary function.
+///
+/// Enables the use of partial application of functions.
+/// This often leads to more concise function declarations.
+/// ```dart
+/// final addFunction = (int a, int b) => a + b;
+/// final add = curry(addFunction);
+///
+/// [1, 2, 3].map(add(1))  // returns [2, 3, 4]
+/// ```
+C Function(B b) Function(A a) curry2<A, B, C>(C Function(A a, B b) function) {
+  return (A a) => (B b) => function(a, b);
+}
+
+/// Converts a ternary function into unary functions.
+D Function(C c) Function(B b) Function(A a) curry3<A, B, C, D>(
+    D Function(A a, B b, C c) function) {
+  return (A a) => (B b) => (C c) => function(a, b, c);
+}
+
+/// Converts a 4 parameters function into unary functions.
+E Function(D d) Function(C c) Function(B b) Function(A a) curry4<A, B, C, D, E>(
+    E Function(A a, B b, C c, D d) function) {
+  return (A a) => (B b) => (C c) => (D d) => function(a, b, c, d);
+}
+
+/// Converts a 5 parameters function into unary functions.
+F Function(E e) Function(D d) Function(C c) Function(B b) Function(A a)
+    curry5<A, B, C, D, E, F>(F Function(A a, B b, C c, D d, E e) function) {
+  return (A a) => (B b) => (C c) => (D d) => (E e) => function(a, b, c, d, e);
+}
