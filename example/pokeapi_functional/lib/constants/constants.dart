440cbb204deb1ff9817a7aaa95178bc5bac82fbf
jbetancourt <josef.betancourt@gmail.com>
2021-12-25 09:31:15 -0500

    ignore avoid_classes_with_only_static_members

--- example/pokeapi_functional/lib/constants/constants.dart
+++ example/pokeapi_functional/lib/constants/constants.dart
@@ -1,4 +1,6 @@
 /// App constants under 'Constants' namespace.
+// ignore_for_file: avoid_classes_with_only_static_members
+
 abstract class Constants {
   static const int minimumPokemonId = 1;
   static const int maximumPokemonId = 898;
