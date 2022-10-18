7a83ab688a0eb3b2ae99f8a8b42a8ead331d2800
SandroMaglione <lass.maglio@gmail.com>
2022-10-18 05:46:25 +0200

    completed tests for meteo location

--- example/open_meteo_api/test/open_meteo_api_client_test_fpdart.dart
+++ example/open_meteo_api/test/open_meteo_api_client_test_fpdart.dart
@@ -112,7 +112,14 @@ void main() {
         final result = await apiClient.locationSearch(query).run();
 
         _isLeftOfType<OpenMeteoApiFpdartLocationFailure, Location,
-            LocationInvalidJsonDecodeFpdartFailure>(result);
+            LocationInvalidJsonDecodeFpdartFailure>(
+          result,
+          typeMatch: (m) => m.having(
+            (failure) => failure.body,
+            'body',
+            '_{}_',
+          ),
+        );
       });
 
       test('returns LocationInvalidMapFpdartFailure when response is not a Map',
@@ -125,7 +132,14 @@ void main() {
         final result = await apiClient.locationSearch(query).run();
 
         _isLeftOfType<OpenMeteoApiFpdartLocationFailure, Location,
-            LocationInvalidMapFpdartFailure>(result);
+            LocationInvalidMapFpdartFailure>(
+          result,
+          typeMatch: (m) => m.having(
+            (failure) => failure.json,
+            'json',
+            [],
+          ),
+        );
       });
 
       test(
@@ -153,7 +167,14 @@ void main() {
         final result = await apiClient.locationSearch(query).run();
 
         _isLeftOfType<OpenMeteoApiFpdartLocationFailure, Location,
-            LocationInvalidListFpdartFailure>(result);
+            LocationInvalidListFpdartFailure>(
+          result,
+          typeMatch: (m) => m.having(
+            (failure) => failure.value,
+            'value',
+            {},
+          ),
+        );
       });
 
       test('returns LocationDataNotFoundFpdartFailure on empty response',
@@ -224,92 +245,5 @@ void main() {
         );
       });
     });
-
-//     group('getWeather', () {
-//       const latitude = 41.85003;
-//       const longitude = -87.6500;
-
-//       test('makes correct http request', () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn('{}');
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         try {
-//           await apiClient.getWeather(latitude: latitude, longitude: longitude);
-//         } catch (_) {}
-//         verify(
-//           () => httpClient.get(
-//             Uri.https('api.open-meteo.com', 'v1/forecast', {
-//               'latitude': '$latitude',
-//               'longitude': '$longitude',
-//               'current_weather': 'true'
-//             }),
-//           ),
-//         ).called(1);
-//       });
-
-//       test('throws WeatherRequestFailure on non-200 response', () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(400);
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         expect(
-//           () async => apiClient.getWeather(
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//           throwsA(isA<WeatherRequestFailure>()),
-//         );
-//       });
-
-//       test('throws WeatherNotFoundFailure on empty response', () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn('{}');
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         expect(
-//           () async => apiClient.getWeather(
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//           throwsA(isA<WeatherNotFoundFailure>()),
-//         );
-//       });
-
-//       test('returns weather on valid response', () async {
-//         final response = MockResponse();
-//         when(() => response.statusCode).thenReturn(200);
-//         when(() => response.body).thenReturn(
-//           '''
-// {
-// "latitude": 43,
-// "longitude": -87.875,
-// "generationtime_ms": 0.2510547637939453,
-// "utc_offset_seconds": 0,
-// "timezone": "GMT",
-// "timezone_abbreviation": "GMT",
-// "elevation": 189,
-// "current_weather": {
-// "temperature": 15.3,
-// "windspeed": 25.8,
-// "winddirection": 310,
-// "weathercode": 63,
-// "time": "2022-09-12T01:00"
-// }
-// }
-//         ''',
-//         );
-//         when(() => httpClient.get(any())).thenAnswer((_) async => response);
-//         final actual = await apiClient.getWeather(
-//           latitude: latitude,
-//           longitude: longitude,
-//         );
-//         expect(
-//           actual,
-//           isA<Weather>()
-//               .having((w) => w.temperature, 'temperature', 15.3)
-//               .having((w) => w.weatherCode, 'weatherCode', 63.0),
-//         );
-//       });
-//     });
   });
 }
