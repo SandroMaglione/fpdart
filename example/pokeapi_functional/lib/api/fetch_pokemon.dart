e3b3fba2ef5da275a2a86cbdeb589924f83051e6
utamori <mori_yuta@retail-ai.jp>
2023-03-14 11:00:18 +0900

    move fetchRamdomPokemon

--- example/pokeapi_functional/lib/api/fetch_pokemon.dart
+++ example/pokeapi_functional/lib/api/fetch_pokemon.dart
@@ -52,3 +52,8 @@ TaskEither<String, Pokemon> fetchPokemon(int pokemonId) => TaskEither.tryCatch(
 /// All the functions are simply chained together following the principle of composability.
 TaskEither<String, Pokemon> fetchPokemonFromUserInput(String pokemonId) =>
     _validateUserPokemonId(pokemonId).flatMapTask(fetchPokemon);
+
+TaskEither<String, Pokemon> fetchRandomPokemon() => fetchPokemon(randomInt(
+      Constants.minimumPokemonId,
+      Constants.maximumPokemonId + 1,
+    ).run());
--- example/pokeapi_functional/lib/controllers/pokemon_provider.dart
+++ example/pokeapi_functional/lib/controllers/pokemon_provider.dart
@@ -2,7 +2,6 @@ import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../api/fetch_pokemon.dart';
-import '../constants/constants.dart';
 import '../models/pokemon.dart';
 
 part 'pokemon_provider.g.dart';
@@ -10,32 +9,13 @@ part 'pokemon_provider.g.dart';
 @riverpod
 class PokemonState extends _$PokemonState {
   @override
-  FutureOr<Pokemon> build() async {
-    final poke = (await fetchPokemon(
-      randomInt(
-        Constants.minimumPokemonId,
-        Constants.maximumPokemonId + 1,
-      ).run(),
-    ).run());
-
-    return poke.match((l) => throw l, (r) => r);
-  }
-
-  /// Initial request, fetch random pokemon passing the pokemon id.
-  Future<Unit> fetchRandom() async => _pokemonRequest(
-        () => fetchPokemon(
-          randomInt(
-            Constants.minimumPokemonId,
-            Constants.maximumPokemonId + 1,
-          ).run(),
-        ),
-      );
+  FutureOr<Pokemon> build() async => (await fetchRandomPokemon().run())
+      .match((error) => throw error, (pokemon) => pokemon);
 
   /// User request, try to convert user input to [int] and then
   /// request the pokemon if successful.
-  Future<Unit> fetch(String pokemonId) async => _pokemonRequest(
-        () => fetchPokemonFromUserInput(pokemonId),
-      );
+  Future<Unit> fetch(String pokemonId) async =>
+      _pokemonRequest(() => fetchPokemonFromUserInput(pokemonId));
 
   /// Generic private method to perform request and update the state.
   Future<Unit> _pokemonRequest(
