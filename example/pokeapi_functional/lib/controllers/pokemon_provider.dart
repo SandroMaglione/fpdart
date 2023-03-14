119b53d454a705f54eeb80d2fd4373b98bc699b0
utamori <mori_yuta@retail-ai.jp>
2023-03-14 11:05:44 +0900

    fix format

--- example/pokeapi_functional/lib/controllers/pokemon_provider.dart
+++ example/pokeapi_functional/lib/controllers/pokemon_provider.dart
@@ -14,8 +14,9 @@ class PokemonState extends _$PokemonState {
 
   /// User request, try to convert user input to [int] and then
   /// request the pokemon if successful.
-  Future<Unit> fetch(String pokemonId) async =>
-      _pokemonRequest(() => fetchPokemonFromUserInput(pokemonId));
+  Future<Unit> fetch(String pokemonId) async => _pokemonRequest(
+        () => fetchPokemonFromUserInput(pokemonId),
+      );
 
   /// Generic private method to perform request and update the state.
   Future<Unit> _pokemonRequest(
@@ -24,8 +25,9 @@ class PokemonState extends _$PokemonState {
     state = AsyncLoading();
     final pokemon = request();
     state = (await pokemon.run()).match(
-        (error) => AsyncError(error, StackTrace.current),
-        (pokemon) => AsyncData(pokemon));
+      (error) => AsyncError(error, StackTrace.current),
+      (pokemon) => AsyncData(pokemon),
+    );
     return unit;
   }
 }
