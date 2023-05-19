f154aca58aa68ff45e32a73b047897b5e5959efe
SandroMaglione <lass.maglio@gmail.com>
2023-05-19 19:39:22 +0200

    update docs to records

--- packages/fpdart/example/src/state_async/state_async1.dart
+++ packages/fpdart/example/src/state_async/state_async1.dart
@@ -4,10 +4,7 @@ Future<int> add10(int previous) async => previous + 10;
 
 Future<void> main() async {
   final stateAsync = StateAsync<int, Unit>(
-    (state) async => tuple2(
-      unit,
-      await add10(state),
-    ),
+    (state) async => (unit, await add10(state)),
   );
 
   final result = await stateAsync.execute(10);
