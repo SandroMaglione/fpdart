0fa8380198e648f284801e60863fee67aeb979a7
SandroMaglione <lass.maglio@gmail.com>
2023-05-19 18:02:42 +0200

    updated docs for curry

--- packages/fpdart/lib/src/typedef.dart
+++ packages/fpdart/lib/src/typedef.dart
@@ -1,5 +1,6 @@
 import 'tuple.dart';
 import 'typeclass/hkt.dart';
 
+typedef Endo<A> = A Function(A a);
 typedef Separated<KT, A, B> = Tuple2<HKT<KT, A>, HKT<KT, B>>;
 typedef Magma<T> = T Function(T x, T y);
