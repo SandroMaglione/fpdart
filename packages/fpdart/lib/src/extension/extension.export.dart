5b38b6428ac63099b1e22ecec31f2b9fbfc5a603
SandroMaglione <lass.maglio@gmail.com>
2023-05-19 18:40:27 +0200

    removed nullable extension

--- packages/fpdart/lib/src/extension/extension.export.dart
+++ packages/fpdart/lib/src/extension/extension.export.dart
@@ -3,6 +3,5 @@ export 'curry_extension.dart';
 export 'date_time_extension.dart';
 export 'list_extension.dart';
 export 'map_extension.dart';
-export 'nullable_extension.dart';
 export 'option_extension.dart';
 export 'string_extension.dart';
