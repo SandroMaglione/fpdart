cfe180efd3d502293b3142128fb292b4a74cc220
SandroMaglione <lass.maglio@gmail.com>
2023-05-19 17:54:09 +0200

    curry and curryAll extensions

--- packages/fpdart/lib/src/extension/curry_extension.dart
+++ packages/fpdart/lib/src/extension/curry_extension.dart
@@ -1,3 +1,7 @@
+/// {@template fpdart_curry_extension}
+/// Extract first parameter from this function to allow curring.
+/// {@endtemplate}
+
 extension CurryExtension2<Input1, Input2, Output> on Output Function(
     Input1, Input2) {
   /// Convert this function from accepting two parameters to a function
@@ -24,8 +28,12 @@ extension CurryExtension3<Input1, Input2, Input3, Output> on Output Function(
   /// of functions that all accept one parameter.
   ///
   /// Inverse of `uncurry`.
-  Output Function(Input3) Function(Input2) Function(Input1) get curry =>
+  Output Function(Input3) Function(Input2) Function(Input1) get curryAll =>
       (input1) => (input2) => (input3) => this(input1, input2, input3);
+
+  /// {@macro fpdart_curry_extension}
+  Output Function(Input2, Input3) Function(Input1) get curry =>
+      (input1) => (input2, input3) => this(input1, input2, input3);
 }
 
 extension UncurryExtension3<Input1, Input2, Input3, Output>
@@ -45,8 +53,13 @@ extension CurryExtension4<Input1, Input2, Input3, Input4, Output> on Output
   ///
   /// Inverse of `uncurry`.
   Output Function(Input4) Function(Input3) Function(Input2) Function(Input1)
-      get curry => (input1) => (input2) =>
+      get curryAll => (input1) => (input2) =>
           (input3) => (input4) => this(input1, input2, input3, input4);
+
+  /// {@macro fpdart_curry_extension}
+  Output Function(Input2, Input3, Input4) Function(Input1) get curry =>
+      (input1) =>
+          (input2, input3, input4) => this(input1, input2, input3, input4);
 }
 
 extension UncurryExtension4<Input1, Input2, Input3, Input4, Output>
@@ -69,8 +82,13 @@ extension CurryExtension5<Input1, Input2, Input3, Input4, Input5, Output>
   /// Inverse of `uncurry`.
   Output Function(Input5) Function(Input4) Function(Input3) Function(Input2)
           Function(Input1)
-      get curry => (input1) => (input2) => (input3) =>
+      get curryAll => (input1) => (input2) => (input3) =>
           (input4) => (input5) => this(input1, input2, input3, input4, input5);
+
+  /// {@macro fpdart_curry_extension}
+  Output Function(Input2, Input3, Input4, Input5) Function(Input1) get curry =>
+      (input1) => (input2, input3, input4, input5) =>
+          this(input1, input2, input3, input4, input5);
 }
 
 extension UncurryExtension5<Input1, Input2, Input3, Input4, Input5, Output>
