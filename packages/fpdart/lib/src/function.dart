04de8efff3835ba54deab053f3f59ad5d7d22712
SandroMaglione <lass.maglio@gmail.com>
2023-05-19 18:02:42 +0200

    updated docs for curry

--- packages/fpdart/lib/src/function.dart
+++ packages/fpdart/lib/src/function.dart
@@ -2,8 +2,6 @@ import 'either.dart';
 import 'extension/string_extension.dart';
 import 'option.dart';
 
-typedef Endo<A> = A Function(A a);
-
 /// Returns the given `a`.
 ///
 /// Same as `id`.
@@ -90,75 +88,6 @@ T2 idSecond<T1, T2>(T1 t1, T2 t2) => t2;
 /// print(c(112.12)); // -> 10
 /// ```
 A Function(dynamic b) constF<A>(A a) => (dynamic b) => a;
-
-/// Converts a binary function into a unary function that returns a unary function.
-///
-/// Enables the use of partial application of functions.
-/// This often leads to more concise function declarations.
-///
-/// The generic types are in this order:
-/// 1. Type of the first function parameter
-/// 2. Type of the second function parameter
-/// 3. Return type of the function
-/// ```dart
-/// final addFunction = (int a, int b) => a + b;
-/// final add = curry2(addFunction);
-///
-/// [1, 2, 3].map(add(1))  // returns [2, 3, 4]
-/// ```
-ReturnType Function(SecondParameter second) Function(FirstParameter first)
-    curry2<FirstParameter, SecondParameter, ReturnType>(
-  ReturnType Function(FirstParameter first, SecondParameter second) function,
-) =>
-        (FirstParameter first) =>
-            (SecondParameter second) => function(first, second);
-
-/// Converts a ternary function into unary functions.
-D Function(C c) Function(B b) Function(A a) curry3<A, B, C, D>(
-    D Function(A a, B b, C c) function) {
-  return (A a) => (B b) => (C c) => function(a, b, c);
-}
-
-/// Converts a 4 parameters function into unary functions.
-E Function(D d) Function(C c) Function(B b) Function(A a) curry4<A, B, C, D, E>(
-    E Function(A a, B b, C c, D d) function) {
-  return (A a) => (B b) => (C c) => (D d) => function(a, b, c, d);
-}
-
-/// Converts a 5 parameters function into unary functions.
-F Function(E e) Function(D d) Function(C c) Function(B b) Function(A a)
-    curry5<A, B, C, D, E, F>(F Function(A a, B b, C c, D d, E e) function) {
-  return (A a) => (B b) => (C c) => (D d) => (E e) => function(a, b, c, d, e);
-}
-
-/// Converts a unary function that returns a unary function into a binary function.
-/// ```dart
-/// final function = (int a) => (int b) => a + b;
-/// final uncurried = uncurry(function);
-/// uncurried(2, 3)  // returns 5
-/// ```
-C Function(A a, B b) uncurry2<A, B, C>(C Function(B a) Function(A a) function) {
-  return (A a, B b) => function(a)(b);
-}
-
-/// Converts a series of unary functions into a ternary function.
-D Function(A a, B b, C c) uncurry3<A, B, C, D>(
-    D Function(C c) Function(B b) Function(A a) function) {
-  return (A a, B b, C c) => function(a)(b)(c);
-}
-
-/// Converts a series of unary functions into a 4 parameters function.
-E Function(A a, B b, C c, D d) uncurry4<A, B, C, D, E>(
-    E Function(D d) Function(C c) Function(B b) Function(A a) function) {
-  return (A a, B b, C c, D d) => function(a)(b)(c)(d);
-}
-
-/// Converts a series of unary functions into a 5 parameters function.
-F Function(A a, B b, C c, D d, E e) uncurry5<A, B, C, D, E, F>(
-    F Function(E e) Function(D d) Function(C c) Function(B b) Function(A a)
-        function) {
-  return (A a, B b, C c, D d, E e) => function(a)(b)(c)(d)(e);
-}
 
 /// {@template fpdart_string_extension_to_num_option}
 /// Convert this [String] to [num], returns [None] for invalid inputs.
