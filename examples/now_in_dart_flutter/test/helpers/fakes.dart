65f702d83dcefb98639442eb0a51dbb7ad5225f7
Biplab Dutta <biplabdutta860@gmail.com>
2023-12-08 04:15:38 +0545

    add mocks and fakes used by the tests

---
+++ examples/now_in_dart_flutter/test/helpers/fakes.dart
@@ -0,0 +1,7 @@
+part of 'mocks.dart';
+
+class FakeUri extends Fake implements Uri {}
+
+class FakeOptions extends Fake implements Options {}
+
+class FakeGithubHeader extends Fake implements GithubHeader {}
