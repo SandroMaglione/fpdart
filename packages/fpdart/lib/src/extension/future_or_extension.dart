8a0f930c8aa8f007ebed30b56db797ac5064ca27
SandroMaglione <lass.maglio@gmail.com>
2024-03-25 06:21:54 +0900

    errors in `FutureOr` sync

--- packages/fpdart/lib/src/extension/future_or_extension.dart
+++ packages/fpdart/lib/src/extension/future_or_extension.dart
@@ -2,11 +2,22 @@ import 'dart:async';
 
 extension FutureOrThenExtension<A> on FutureOr<A> {
   FutureOr<B> then<B>(FutureOr<B> Function(A a) f,
-          {B Function(Object error)? onError}) =>
-      switch (this) {
-        final Future<A> self => self.then(f, onError: (Object error) {
+      {B Function(Object error)? onError}) {
+    switch (this) {
+      case Future<A> self:
+        return self.then(
+          f,
+          onError: (Object error) {
             if (onError != null) onError(error);
-          }),
-        final A self => f(self),
-      };
+          },
+        );
+      case A self:
+        try {
+          return f(self);
+        } catch (error) {
+          if (onError != null) return onError(error);
+          rethrow;
+        }
+    }
+  }
 }
